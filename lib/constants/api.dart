dcaebd00757366b0793ea0afe91a639bcfbc7b1d
MarianneLeigh <marianneleighreyes27@gmail.com>
2023-05-23 11:47:51 +0800

    hi

--- lib/constants/api.dart
+++ lib/constants/api.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   static String baseUrl = "http://192.168.1.10:8000/api";
   // static String baseEmuUrl = "http://10.0.2.2:8000/api";
-  static String socketBase = "http://192.168.1.10:8002";
+  static String socketBase = "http://192.168.110.192:8002";
 
-  static String baseEmuUrl = "http://192.168.1.10:8000/api";
+  static String baseEmuUrl = "http://192.168.110.192:8000/api";
 }
